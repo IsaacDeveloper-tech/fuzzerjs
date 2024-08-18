4eb31402d5aa2a60285f0371db003c6016405a60
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2024-08-18 21:25:56 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { args } from "./plugins/args.plugin";
+
+function main():void
+{
+    console.log(args);
+}
+
+main();
---
+++ src/plugins/csv.plugin.ts
@@ -0,0 +1,3 @@
+import * as csvlib from "../../node_modules/csv/lib/index";
+
+export const csv = csvlib;
