cb592daac4484d9a8eb317b595b8b461d7a1f5f4
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2024-08-18 22:37:03 +0200

    basic configuration for node project

--- src/plugins/args.plugin.ts
+++ src/plugins/args.plugin.ts
@@ -1,3 +1,10 @@
-import yargs from "yargs";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
-export const args = yargs(process.argv);
+export const args = yargs(hideBin(process.argv))
+.option('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'Run with verbose logging'
+})
+.parse();
